f288569091af0734d1e9471f0254d44facfb86f6
Xiaoxi <yxx0827@gmail.com>
2023-05-18 01:51:18 -0400

    update

--- src/utils/formikHelpers.ts
+++ src/utils/formikHelpers.ts
@@ -1,7 +1,9 @@
 import startCase from "lodash/startCase";
+import {Formik} from "formik";
+
 const getLabel = (key: string) => startCase(key);
 
-const getTextFieldPropsFromFormik = (key: string, formik: any) => {
+const getTextFieldPropsFromFormik = (key: string, formik: Formik) => {
   return {
     id: key,
     name: key,
